77ec902e4e580880e9513c14375edb1b332948d7
VibhuSVK <45452967+VibhuSVK@users.noreply.github.com>
2019-06-06 10:35:25 -0700

    Add files via upload
    kermit

---
+++ core/assets/Maps/Orbit Maps/Maps/tileset_base.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tileset" tilewidth="32" tileheight="32" tilecount="28" columns="7">
+ <image source="../../TileSet/HomeBase/TileSet.png" trans="ffffff" width="224" height="128"/>
+</tileset>
