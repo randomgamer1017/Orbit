91145d7452c8e3cceb0d033b9631f429205abb70
VibhuSVK <45452967+VibhuSVK@users.noreply.github.com>
2019-06-07 09:34:26 -0700

    Add files via upload
    i want to die

---
+++ core/assets/Maps/Orbit Maps/Maps/tileSet_planet1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tileSet_planet" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../TileSet/Planet 1/tileSet_planet.png" trans="ffffff" width="96" height="96"/>
+</tileset>
