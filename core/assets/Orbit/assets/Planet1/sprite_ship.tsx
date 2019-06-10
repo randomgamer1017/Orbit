813a5583af8af1c78cd9b874cd932262a086abc8
VibhuSVK <45452967+VibhuSVK@users.noreply.github.com>
2019-06-10 04:36:00 -0700

    Add files via upload
    sepuku

---
+++ core/assets/Orbit/assets/Planet1/sprite_ship.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="sprite_ship" tilewidth="32" tileheight="32" tilecount="102" columns="17">
+ <image source="../../sprites/sprite_ship.png" trans="ffffff" width="558" height="198"/>
+</tileset>
